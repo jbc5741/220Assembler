e68e1c03451d431449df332be9140b0b056b4dd7
Jesse Cox <jbc5741@gmail.com>
2019-03-31 16:38:18 -0700

    Write initial code for Code and SymbolTable

--- src/Code.java
+++ src/Code.java
@@ -1,2 +1,90 @@
+import java.util.HashMap;
+
+/**
+ * Code.java : Translates Hack assembly language mnemonics into binary codes.
+ *
+ * @author Jesse Cox
+ * @version 1.0
+ */
+
+
+
 public class Code {
+    // INSTANCE VARIABLES
+    private HashMap<String, String> compCodes;
+    private HashMap<String, String> destCodes;
+    private HashMap<String, String> jumpCodes;
+
+    // SIMPLE CONSTRUCTOR
+    /**
+     * Initializes hashmaps with binary codes for easy lookup.
+     */
+    public Code() {
+        compCodes = new HashMap<>(28);
+        destCodes = new HashMap<>(8);
+        jumpCodes = new HashMap<>(8);
+
+        // COMP INITIALIZATION
+        compCodes.put("0","0101010");       compCodes.put("1","0111111");
+        compCodes.put("-1","0111010");      compCodes.put("D","0001100");
+        compCodes.put("A","0110000");       compCodes.put("!D","0001101");
+        compCodes.put("!A","0110001");      compCodes.put("-D","0001111");
+        compCodes.put("-A","0110011");      compCodes.put("D+1","0011111");
+        compCodes.put("A+1","0110111");     compCodes.put("D-1","0001110");
+        compCodes.put("A-1","0110010");     compCodes.put("D+A","0000010");
+        compCodes.put("D-A","0010011");     compCodes.put("A-D","0000111");
+        compCodes.put("D&A","0000000");     compCodes.put("D|A","0010101");
+        compCodes.put("M","1110000");       compCodes.put("!M","1110001");
+        compCodes.put("-M","1110011");      compCodes.put("M+1","1110111");
+        compCodes.put("M-1","1110010");     compCodes.put("D+M","1000010");
+        compCodes.put("D-M","1010011");     compCodes.put("M-D","1000111");
+        compCodes.put("D&M","1000000");     compCodes.put("D|M","1010101");
+
+        // DEST INITIALIZATION
+        destCodes.put("null","000");        destCodes.put("M","001");
+        destCodes.put("D","010");           destCodes.put("MD","011");
+        destCodes.put("A","100");           destCodes.put("AM","101");
+        destCodes.put("AD","110");          destCodes.put("AMD","111");
+
+        // JUMP INITIALIZATION
+        jumpCodes.put("null","000");        jumpCodes.put("JGT","001");
+        jumpCodes.put("JEQ","010");         jumpCodes.put("JGE","011");
+        jumpCodes.put("JLT","100");         jumpCodes.put("JNE","101");
+        jumpCodes.put("JLE","101");         jumpCodes.put("JMP","111");
+    }
+
+    // ACCESSOR METHODS
+
+    /**
+     * Converts a mnemonic string to a string of 7 bits.
+     *
+     * @param mnemonic
+     *          String that represents the mnemonic for a comp code.
+     * @return String held by compCodes HashTable for the given mnemonic String key.
+     */
+    public String getComp(String mnemonic) {
+        return compCodes.get(mnemonic);
+    }
+
+    /**
+     * Convertsa  mnemonic String to a String of 3 bits.
+     *
+     * @param mnemonic
+     *          String that represents the mnemonic for a dest code.
+     * @return String held by destCodes HashTable for the given mnemonic String key.
+     */
+    public String getDest(String mnemonic) {
+        return destCodes.get(mnemonic);
+    }
+
+    /**
+     * Converts a mnemonic String to a String of 3 bits.
+     *
+     * @param mnemonic
+     *          String that represents the mnemonic for a jump code.
+     * @return String held by jumpCodes HashTable for the given mnemonic String key.
+     */
+    public String getJump(String mnemonic) {
+        return jumpCodes.get(mnemonic);
+    }
 }
--- src/SymbolTable.java
+++ src/SymbolTable.java
@@ -1,2 +1,103 @@
+import java.util.HashMap;
+
+/**
+ * SymbolTable.java : Manages the symbol table. Keeps a correspondence between
+ *                    symbolic labels and numeric addresses.
+ *
+ * @author Jesse Cox
+ * @version 1.0
+ */
+
 public class SymbolTable {
+
+    // DEFAULT VALUES
+    private static final String INITIAL_VALID_CHARS = "";
+    private static final String ALL_VALID_CHARS =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_";
+
+    // SINGLE INSTANCE VARIABLE
+    private HashMap<String, Integer> symbolTable;
+
+    // SIMPLE CONSTRUCTOR
+    /**
+     * Initializes hashmap with predefined symbols.
+     */
+    public SymbolTable() {
+        symbolTable = new HashMap<>();
+
+        // SYMBOLTABLE INITIALIZATION
+        symbolTable.put("R0",0);        symbolTable.put("R1",1);
+        symbolTable.put("R2",2);        symbolTable.put("R3",3);
+        symbolTable.put("R4",4);        symbolTable.put("R5",5);
+        symbolTable.put("R6",6);        symbolTable.put("R7",7);
+        symbolTable.put("R8",8);        symbolTable.put("R9",9);
+        symbolTable.put("R10",10);      symbolTable.put("R11",11);
+        symbolTable.put("R12",12);      symbolTable.put("R13",13);
+        symbolTable.put("R14",14);      symbolTable.put("R15",15);
+
+        symbolTable.put("SCREEN",16384);
+        symbolTable.put("KBD",24576);   symbolTable.put("SP",0);
+        symbolTable.put("LCL",1);       symbolTable.put("ARG",2);
+        symbolTable.put("THIS",3);      symbolTable.put("THAT",4);
+        symbolTable.put("WRITE",18);    symbolTable.put("END",22);
+        symbolTable.put("i",16);        symbolTable.put("sum",17);
+    }
+
+    /**
+     * Adds new pair of symbol / address to the symbolTable HashMap.
+     *
+     * @param symbol
+     *          String representing a new symbol to add to the Hashmap.
+     * @param address
+     *          integer value for the address of the new symbol to be added.
+     * @return true if able to add, false if illegal name.
+     */
+    public boolean addEntry(String symbol, int address) {
+        if (!(this.contains(symbol))) {
+            symbolTable.put(symbol,address);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Returns boolean of whether the symbolTable contains a given symbol.
+     *
+     * @param symbol
+     *          String representing the symbol to be checked.
+     * @return True if this symbolTable contains the given symbol, otherwise false.
+     */
+    public boolean contains(String symbol) {
+        return symbolTable.containsKey(symbol);
+    }
+
+    /**
+     * Returns address in hashmap of given symbol
+     *
+     * @param symbol
+     *          String representing the symbol to be found.
+     * @return address of the given symbol.
+     */
+    public int getAddress(String symbol) {
+        return symbolTable.get(symbol);
+    }
+
+    /**
+     * Returns boolean of whether the symbol is a valid symbol name or not.
+     *
+     * @param symbol
+     *          String representing the symbol to be checked if valid or not.
+     * @return True if the symbol is valid, false otherwise.
+     */
+    public boolean isValidName(String symbol) {
+        boolean toReturn = true;
+        char[] charray = symbol.toCharArray();
+        for (char c : charray) {
+            if (!(ALL_VALID_CHARS.contains(""+c))) {
+                toReturn = false;
+            }
+        }
+        return toReturn;
+    }
 }
